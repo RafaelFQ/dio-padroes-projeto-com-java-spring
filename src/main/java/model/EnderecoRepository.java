9ba1aca2a84d89c8dc99d44055b25a8f60dcc2be
Rafael <rafaelmetallica@gmail.com>
2024-07-15 18:38:25 -0300

    primeiro commit

---
+++ src/main/java/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package model;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String>  {
+
+}
---
+++ src/test/java/lab_padroes_projeto_spring/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lab_padroes_projeto_spring.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
